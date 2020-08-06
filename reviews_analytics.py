37c447b72d813f0ccac3db1670d0bc7079c1565a
Eric Yeh <e122426@gmail.com>
2020-08-07 00:12:46 +0800

    add basic filtering function

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -11,3 +11,15 @@ for d in data:
     sum_len += len(d)
 print("其中檔案總共有", sum_len, "個字")
 print("平均留言長度為", sum_len/len(data), "字")
+
+new = []
+for d in data:
+    if len(d) < 100:
+    	new.append(d)
+print("\n一共有", len(new), "筆資料留言字數小於100")
+
+good = []
+for d in data:
+    if "good" in d:
+    	good.append(d)
+print("而在留言中有提到'good'一詞的共有", len(good), "筆")
